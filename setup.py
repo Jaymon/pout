b2c76bc336cde896f6c19ea408d2003ee4f51be8
Jay Marcyes <jay@marcyes.com>
2019-11-20 17:02:52 -0700

    updates setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
-# http://docs.python.org/distutils/setupscript.html
-# http://docs.python.org/2/distutils/examples.html
-
 from setuptools import setup, find_packages
 import re
 import os
@@ -10,27 +7,37 @@ from codecs import open
 
 name = "pout"
 
+kwargs = {"name": name}
+
 def read(path):
     if os.path.isfile(path):
         with open(path, encoding='utf-8') as f:
             return f.read()
     return ""
 
-path = os.path.join(name, "__init__.py")
-version = re.search("^__version__\s*=\s*[\'\"]([^\'\"]+)", read(path), flags=re.I | re.M).group(1)
 
-long_description = read('README.rst')
+vpath = os.path.join(name, "__init__.py")
+if os.path.isfile(vpath):
+    kwargs["packages"] = find_packages(exclude=["tests", "tests.*", "*_test*", "examples"])
+else:
+    vpath = "{}.py".format(name)
+    kwargs["py_modules"] = [name]
+kwargs["version"] = re.search(r"^__version__\s*=\s*[\'\"]([^\'\"]+)", read(vpath), flags=re.I | re.M).group(1)
+
+
+# https://pypi.org/help/#description-content-type
+kwargs["long_description"] = read('README.md')
+kwargs["long_description_content_type"] = "text/markdown"
+
+kwargs["tests_require"] = ["testdata"]
+kwargs["install_requires"] = []
+
 
 setup(
-    name=name,
-    version=version,
     description='Prints out python variables in an easy to read way, handy for debugging',
-    long_description=long_description,
     author='Jay Marcyes',
     author_email='jay@marcyes.com',
     url='http://github.com/Jaymon/{}'.format(name),
-    #py_modules=[name],
-    packages=find_packages(),
     license="MIT",
     classifiers=[ # https://pypi.python.org/pypi?:action=list_classifiers
         'Development Status :: 4 - Beta',
@@ -42,7 +49,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
     ],
-    tests_require=['testdata'],
     #test_suite = "pout_test",
     entry_points = {
         'console_scripts': [
@@ -51,5 +57,6 @@ setup(
 #             '{}.inject = {}.__main__:main_inject'.format(name, name)
             '{} = {}.__main__:main'.format(name, name)
         ],
-    }
+    },
+    **kwargs
 )
